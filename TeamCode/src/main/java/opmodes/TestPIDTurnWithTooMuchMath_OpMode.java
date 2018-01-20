f48307e4fbe5053776ee0313e281e7b24f359c16
rachel <rachelwu.47@gmail.com>
2018-01-20 10:49:34 -0500

    practically implemented my likely innacurate, unneccessary, PID-aided turn. now to opmode it.

--- TeamCode/src/main/java/opmodes/TestPIDTurnWithTooMuchMath_OpMode.java
+++ TeamCode/src/main/java/opmodes/TestPIDTurnWithTooMuchMath_OpMode.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.Robot2017;
+import org.firstinspires.ftc.teamcode.commands.NormalTurnButIinjectedItWithPIDToMakeItAMoreAccurateCrappyTurn;
 
 /**
  * Created by rachel on 1/19/2018.
@@ -16,18 +17,26 @@ public class TestPIDTurnWithTooMuchMath_OpMode extends LinearOpMode
     public void runOpMode() throws InterruptedException {
 
         Robot2017 samuel = new Robot2017(hardwareMap);
-
+        NormalTurnButIinjectedItWithPIDToMakeItAMoreAccurateCrappyTurn testright = new NormalTurnButIinjectedItWithPIDToMakeItAMoreAccurateCrappyTurn(90,1, samuel, this);
+        NormalTurnButIinjectedItWithPIDToMakeItAMoreAccurateCrappyTurn testleft = new NormalTurnButIinjectedItWithPIDToMakeItAMoreAccurateCrappyTurn(-90, 1, samuel, this);
 
         telemetry.addData("Status", "started");
         telemetry.update();
 
+        sleep(1000);
 
         telemetry.addData("Status", "should have done nothing");
+        telemetry.update();
+
+        testright.start();
 
         telemetry.addData("Status", "should have turned right");
+        telemetry.update();
+
+        testleft.start();
 
         telemetry.addData("Status", "should have turned left");
-
+        telemetry.update();
 
 
     }
