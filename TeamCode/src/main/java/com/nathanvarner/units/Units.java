8d1eb648915d10f0c6510b72e2540438e2c3e0db
nvarner <nathan@nathanmvarner.com>
2018-08-15 09:49:58 -0400

    Updated unit converter and made the licence more permissive

--- TeamCode/src/main/java/com/nathanvarner/units/Units.java
+++ TeamCode/src/main/java/com/nathanvarner/units/Units.java
@@ -1,6 +1,26 @@
 /*
- * Copyright 2018 Nathan Varner, All Rights Reserved
- */
+MIT License
+
+Copyright (c) 2018 Nathan Varner
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 package com.nathanvarner.units;
 
@@ -32,8 +52,20 @@ public class Units {
 
   // Standard units
   public static Unit degree = new Unit(rotation, 1, 360, "deg");
-  public static Unit radian = new Unit(rotation, 1, 1 / (2 * Math.PI), "rad");
+  public static Unit radian = new Unit(rotation, 1, 2 * Math.PI, "rad");
 
   // Encoders
-  public static Unit tetrixEncoder = new Unit(rotation, 1, 1440, "Tetrix enc");
+  public static Unit tetrixEncoder = new Unit(rotation, 1, 1440, "Tetrix clx");
+
+  /*
+   * Units of temperature
+   * Base unit: °C
+   */
+  public static Unit celsius = new Unit(null, 1, 1, "°C");
+
+  // SI
+  public static Unit kelvin = new Unit(celsius, 1, 1, 273.15, "°K");
+
+  // Imperial
+  public static Unit fahrenheit = new Unit(celsius, 1, 1.8, 32, "°F");
 }
